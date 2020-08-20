1dcab706b7d576a34017b183b9f268434d7b7a1e
denys krychevskyi <dkrychevskyi@gmail.com>
2020-08-20 12:49:42 -0700

    new design

--- product_app/views.py
+++ product_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.core.files.storage import FileSystemStorage
 from .models import *
 # Create your views here.
 # def index(request):
@@ -17,19 +16,10 @@ def checkout(request):
     context = {}
     return render(request, 'checkout.html', context)
 
-def pay(request):
+def item(request):
     context = {}
-    return render(request, 'pay.html', context)
+    return render(request, 'item.html', context)
 
 def create_new_product(request):
-    if request.method == 'GET':
-        return render(request, 'multipic.html')
-    else:
-        # fs=FileSystemStorage(location='media/product_pictures')
-        # print('*'*30)
-        # for picture in request.FILES.getlist('product_image'):
-        #     pic=fs.save(picture.name, picture)
-        #     url=fs.url(pic)
-        # print('*'*30)
-        Product.objects.create_product(request.POST, request.FILES)
-        return redirect('/store')
+    Product.objects.create_product(request.FILES)
+    return redirect('/store')
